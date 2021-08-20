588c317c20bb3f3748463c2064d569c1b26eaba7
Muhammad Nasrurrohman <anas@jurnalanas.com>
2021-08-20 07:13:35 +0700

    adjust landingpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,21 @@ const Index = () => {
         />
       }
     >
-      <h1 className="font-bold text-2xl dark:bg-gray-500">
-        Boilerplate code for your Nextjs project with Tailwind CSS
-      </h1>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+              Sample Hero
+            </h2>
+            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Build something awesome
+            </p>
+            <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+              Start building for free, then add a site plan to go live.
+            </p>
+          </div>
+        </div>
+      </div>
     </Main>
   );
 };
